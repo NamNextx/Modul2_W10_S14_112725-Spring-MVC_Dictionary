4295c890a7ffbc0d8a7c95e75691156be9104d37
Namnextx <phuongnam.nextx@gmail.com>
2019-04-21 14:50:02 +0700

    1. First commit

--- src/main/java/com/codegym/controller.java
+++ src/main/java/com/codegym/controller.java
@@ -5,33 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class controller {
-    @GetMapping("/hello_Nam")
-    public String helloNam(){
-        return "index";
-    }
-    @GetMapping("/hello_Duy")
-    public String helloNam2(){
-        return "index2";
-    }
-    @GetMapping("/hello_Thanh")
-    public String helloThanh(){
-        return "index2";
-    }
-    @GetMapping("/hello_mom")
-    public String helloMom(){
-        return "index";
-    }
-    @GetMapping("/Test")
-    public String testMapping(){
-        return "test";
-    }
-    @GetMapping("/Test1")
-    public String testMapping1(){
-        return "index";
-    }
-    @GetMapping("/test2")
-    public String testMapping2(){
-        return "index3";
+    @GetMapping("/home")
+    public String requestHome() {
+        return "home";
     }
 }
 
